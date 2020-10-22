148b173db276b67afc4f7a37a91750b107683c7d
Dmitry <tarasenko@werbelift.de>
2020-10-22 14:23:51 +0300

    feature(monads): update PostMock

--- src/models/Post/api/PostMock.ts
+++ src/models/Post/api/PostMock.ts
@@ -1,24 +1,26 @@
 import { posts } from '../../../../tests/data/posts'
 import { IPost, PostRepository } from '@/models/Post'
+import { right, left } from '@sweet-monads/either';
+import { PostsError } from '@/errors/PostsError';
 
 export class PostMock implements PostRepository {
   // eslint-disable-next-line, require-await
   public async createPost (post: IPost) {
-    return Promise.resolve(post)
+    return right(post)
   }
 
   public async removePost (postId: string) {
-    return Promise.resolve();
+    return right(null);
   }
 
   public async getPosts () {
-    return Promise.resolve(posts);
+    return right(posts);
   }
 
   public async getPost (postId: string) {
-    const foundPost = posts.find((post: IPost) => post.id == postId)
+    const foundPost: IPost = posts.find((post: IPost) => post.id == postId)!
     if (foundPost) {
-      return Promise.resolve(foundPost);
-    } return Promise.reject();
+      return right(foundPost);
+    } return left(new PostsError(`This post does not exist`));
   }
 }
